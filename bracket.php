a91dbfe2d190bef343a2316bc6186c0e9b8df793
jloveracondor <julio.lovera@condoragency.com>
2020-10-20 11:38:53 -0400

    Bracket

---
+++ bracket.php
@@ -0,0 +1,5 @@
+<?php include("header.php"); ?>
+<?php include("tabla-resultados-superior.php");?>
+<?php include("ads.php");?>
+<?php include("pg-bracket/pg-braket.php");?>
+<?php include ("footer.php"); ?>
