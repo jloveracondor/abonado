4ea3d13d9da655e05b65b33b60ba4dda341ddc0c
jloveracondor <julio.lovera@condoragency.com>
2020-11-04 14:23:36 -0400

    tabs para pitcheo y resultados

--- elementos/tabla-bateo.php
+++ elementos/tabla-bateo.php
@@ -24,11 +24,21 @@
 				<option>BATEO</option>
 			</select>
 		</div>
-		<div class="col-2 align-middle">
-			<span class="ab-regular">Regular</span>
+		<div class="col-8 align-middle">
+			
+			<nav>
+				<div class="nav nav-tabs ab-tabs" id="nav-tab" role="tablist">
+					<a class="nav-item nav-link active" id="nav-regular-tab" data-toggle="tab" href="#nav-regular" role="tab" aria-controls="nav-regular" aria-selected="true">Regular</a>
+					<a class="nav-item nav-link" id="nav-segunda-tab" data-toggle="tab" href="#nav-segunda" role="tab" aria-controls="nav-segunda" aria-selected="false">Segunda Vuelta</a>
+
+				</div>
+			</nav>
 		</div>
 	</div>
-	<div class="row ">
+	<!-- Tabs-->
+	<div class="tab-content" id="nav-tabContent">
+		<div class="tab-pane fade show active" id="nav-regular" role="tabpanel" aria-labelledby="nav-regular-tab">
+		<div class="row ">
 		<div class="col"> 
 			<div class="col rounded border table-responsive">
 				<p class="ab-title-liga">Liga Venezolana de Béisbol Profesional <img src="/abonado/assent/img/image 17.png" alt=""></p>
@@ -252,4 +262,236 @@
 			</div>
 		</div>
 	</div>
+
+		</div>
+		<div class="tab-pane fade" id="nav-segunda" role="tabpanel" aria-labelledby="nav-segunda-tab">
+			<div class="row ">
+		<div class="col"> 
+			<div class="col rounded border table-responsive">
+				<p class="ab-title-liga">Liga Venezolana de Béisbol Profesional <img src="/abonado/assent/img/image 17.png" alt=""></p>
+				<table class="table table-hover ab-table">
+					<thead>
+						<tr class="ab-titulos-table text-center">
+							<th></th>
+							<th  scope="col">Jugador</th>
+							<th scope="col">EQUIPO</th>
+							<th scope="col">JJ</th>
+							<th scope="col">VB</th>
+							<th scope="col">CA</th>
+							<th scope="col">H</th>
+							<th scope="col">2B</th>
+							<th scope="col">3B</th>
+							<th scope="col">HR</th>
+							<th scope="col">CI</th>
+							<th scope="col">BR</th>
+							<th scope="col">OR</th>
+							<th scope="col">SH</th>
+							<th scope="col">SF</th>
+							<th scope="col">BB</th>
+							<th scope="col">K</th>
+							<th class="ab-average" scope="col" data-toggle="tooltip" data-placement="top" title="Promedio Bateo"> AVG </th>
+							<th scope="col"> OBP </th>
+							<th scope="col"> SLG </th>
+							<th scope="col"> OPS </th>
+						</tr>
+					</thead>
+					<tbody class="text-center">
+						<tr><td> 1</td>
+							<td class="text-left" scope="row">Juan Apodaca</td>
+							<td><img src="/abonado/assent/img/LVBP/aguilas-del-zulia 1.png" alt=""> ADZ</td>
+							<td>8</td>
+							<td>21</td>
+							<td>3</td>
+							<td>13</td>
+							<td>4	</td>
+							<td>0</td>
+							<td>0</td>
+							<td>7</td>
+							<td>0</td>
+							<td>0</td>
+							<td>-0</td>
+							<td>1</td>
+							<td>1</td>
+							<td>2</td>
+							<td class="ab-average-fondo">.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							
+							
+							
+						</tr>
+						<tr><td> 1</td>
+							<td class="text-left" scope="row">Juan Apodaca</td>
+							<td><img src="/abonado/assent/img/LVBP/bravos-de-margarita 1.png" alt="">BDM</td>
+							<td>8</td>
+							<td>21</td>
+							<td>3</td>
+							<td>13</td>
+							<td>4	</td>
+							<td>0</td>
+							<td>0</td>
+							<td>7</td>
+							<td>0</td>
+							<td>0</td>
+							<td>-0</td>
+							<td>1</td>
+							<td>1</td>
+							<td>2</td>
+							<td class="ab-average-fondo">.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							
+						</tr>
+						<tr><td> 1</td>
+							<td class="text-left" scope="row">Juan Apodaca</td>
+							<td><img src="/abonado/assent/img/LVBP/cardenales-de-lara 1.png" alt="">CDL</td>
+							<td>8</td>
+							<td>21</td>
+							<td>3</td>
+							<td>13</td>
+							<td>4	</td>
+							<td>0</td>
+							<td>0</td>
+							<td>7</td>
+							<td>0</td>
+							<td>0</td>
+							<td>-0</td>
+							<td>1</td>
+							<td>1</td>
+							<td>2</td>
+							<td class="ab-average-fondo">.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							
+						</tr>
+						<tr><td> 1</td>
+							<td class="text-left" scope="row"> Juan Apodaca</td>
+							<td><img src="/abonado/assent/img/LVBP/caribes-de-anzoategui 1.png">CDA</td>
+							<td>8</td>
+							<td>21</td>
+							<td>3</td>
+							<td>13</td>
+							<td>4	</td>
+							<td>0</td>
+							<td>0</td>
+							<td>7</td>
+							<td>0</td>
+							<td>0</td>
+							<td>-0</td>
+							<td>1</td>
+							<td>1</td>
+							<td>2</td>
+							<td class="ab-average-fondo">.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							
+						</tr>
+						
+						<tr><td> 1</td>
+							<td class="text-left" scope="row"> Juan Apodaca</td>
+							<td><img src="/abonado/assent/img/LVBP/leones-del-caracas 1.png" alt="">LDC</td>
+							<td>8</td>
+							<td>21</td>
+							<td>3</td>
+							<td>13</td>
+							<td>4	</td>
+							<td>0</td>
+							<td>0</td>
+							<td>7</td>
+							<td>0</td>
+							<td>0</td>
+							<td>-0</td>
+							<td>1</td>
+							<td>1</td>
+							<td>2</td>
+							<td class="ab-average-fondo">.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							
+						</tr>
+						
+						<tr><td> 1</td>
+							<td class="text-left" scope="row">Juan Apodaca</td>
+							<td><img src="/abonado/assent/img/LVBP/navegantes-del-magallanes 1.png" alt="">NDM</td>
+							<td>8</td>
+							<td>21</td>
+							<td>3</td>
+							<td>13</td>
+							<td>4	</td>
+							<td>0</td>
+							<td>0</td>
+							<td>7</td>
+							<td>0</td>
+							<td>0</td>
+							<td>-0</td>
+							<td>1</td>
+							<td>1</td>
+							<td>2</td>
+							<td class="ab-average-fondo">.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							
+						</tr>
+						<tr><td> 1</td>
+							<td class="text-left" scope="row">Juan Apodaca</td>
+							<td><img src="/abonado/assent/img/LVBP/tiburones-de-la-guaira 1.png" alt="">TDG</td>
+							<td>8</td>
+							<td>21</td>
+							<td>3</td>
+							<td>13</td>
+							<td>4	</td>
+							<td>0</td>
+							<td>0</td>
+							<td>7</td>
+							<td>0</td>
+							<td>0</td>
+							<td>-0</td>
+							<td>1</td>
+							<td>1</td>
+							<td>2</td>
+							<td class="ab-average-fondo">.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							<td>.400</td>
+						</tr>
+						<tr><td> 1</td>
+							<td class="text-left" scope="row"> Juan Apodaca</td>
+							<td><img src="/abonado/assent/img/LVBP/tigres-de-aragua 1.png" alt="">TGA</td>
+							<td>8</td>
+							<td>21</td>
+							<td>3</td>
+							<td>13</td>
+							<td>4	</td>
+							<td>0</td>
+							<td>0</td>
+							<td>7</td>
+							<td>0</td>
+							<td>0</td>
+							<td>-0</td>
+							<td>1</td>
+							<td>1</td>
+							<td>2</td>
+							<td class="ab-average-fondo">.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							<td>.400</td>
+							
+						</tr>
+					</tbody>
+				</table>
+
+			</div>
+		</div>
+	</div>
+
+		</div>
+
+	</div>
+	
 </div>
