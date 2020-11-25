5f732a6481555a1b67f5953ddb75ca4eaef584bd
Kent <kent.fornica@condoragency.com>
2020-11-25 12:37:15 -0400

    25nov2020

--- pg-bracket/pg-braket.php
+++ pg-bracket/pg-braket.php
@@ -1,34 +1,55 @@
+<?php
+$equipo1 = "Equipo 1";
+$equipo2 = "Equipo 2";
+$equipo3 = "Equipo 3";
+$equipo4 = "Equipo 4";
+$equipo5 = "Equipo 5";
+$equipo6 = "Equipo 6";
+$equipo7 = "Equipo 7";
+$equipo8 = "Equipo 8";
+
+$ganadorPartido1 = "Ganador Partido 1";
+$ganadorPartido2 = "Ganador Partido 2";
+$ganadorPartido3 = "Ganador Partido 3";
+$ganadorPartido4 = "Ganador Partido 4";
+
+$ganadorLadoA = "Ganador Lado A";
+$ganadorLadoB = "Ganador Lado B";
+
+$equipoGanador = "Ganador";
+?>
+
 <section class="container-fluid">
-  <div class="ca-abo-grid ca-abo-template">
+  <div class="ca-abo-web ca-abo-grid ca-abo-template">
 
     <div class="ca-abo-leftRight ca-abo-left">
       <div class="ca-abo-round ca-abo-round1" style="padding: 20px 0;">
         <div class="ca-abo-contenedorBackLeft ca-abo-contenedorBack1">
-          <div class="ca-abo-equipo ca-abo-equipoA" style="left: -1px;">Primer Equipo</div>
-          <div class="ca-abo-equipo ca-abo-equipoB" style="left: -1px;">Segundo Equipo</div>
+          <div class="ca-abo-equipo ca-abo-equipoA" style="left: -1px;"><?php echo $equipo1 ?></div>
+          <div class="ca-abo-equipo ca-abo-equipoB" style="left: -1px;"><?php echo $equipo2 ?></div>
         </div>
         <div class="ca-abo-contenedorBackLeft ca-abo-contenedorBack1">
-          <div class="ca-abo-equipo ca-abo-equipoA" style="left: -1px;">Tercer Equipo</div>
-          <div class="ca-abo-equipo ca-abo-equipoB" style="left: -1px;">Cuarto Equipo</div>
+          <div class="ca-abo-equipo ca-abo-equipoA" style="left: -1px;"><?php echo $equipo3 ?></div>
+          <div class="ca-abo-equipo ca-abo-equipoB" style="left: -1px;"><?php echo $equipo4 ?></div>
         </div>
       </div>
       <div class="ca-abo-round ca-abo-round2" style="padding: 20px 0;">
         <div class="ca-abo-contenedorBackLeft ca-abo-contenedorBack2">
-          <div class="ca-abo-equipo ca-abo-equipoA" style="left: 10px;">Ganador Ronda 1</div>
-          <div class="ca-abo-equipo ca-abo-equipoB" style="left: 10px;">Ganador Ronda 2</div>
+          <div class="ca-abo-equipo ca-abo-equipoA" style="left: 10px;"><?php echo $ganadorPartido1 ?></div>
+          <div class="ca-abo-equipo ca-abo-equipoB" style="left: 10px;"><?php echo $ganadorPartido2 ?></div>
         </div>
       </div>
       <div class="ca-abo-round ca-abo-round3" style="padding: 20px 0;">
         <div class="ca-abo-contenedorBackLeft" style="border-bottom: none; width: 100%;">
-          <div class="ca-abo-equipo ca-abo-equipoA" style="left: initial; right: -2px;">Ganador Lado Izquierdo</div>
+          <div class="ca-abo-equipo ca-abo-equipoA" style="left: initial; right: -2px;"><?php echo $ganadorLadoA ?></div>
         </div>
       </div>
     </div>
 
-    <div class="ca-abo-center" style="display: flex; align-items: center; justify-content: center;">
+    <div class="ca-abo-center" style="display: flex; align-items: center; justify-content: center; padding: 20px 0;">
       <div class="ca-abo-round ca-abo-round3">
         <div class="" style="border-bottom: none;">
-          <div class="ca-abo-equipoGanador">Ganador</div>
+          <div class="ca-abo-equipoGanador"><?php echo $equipoGanador ?></div>
         </div>
       </div>
     </div>
@@ -36,23 +57,23 @@
     <div class="ca-abo-leftRight ca-abo-right">
     <div class="ca-abo-round ca-abo-round3" style="padding: 20px 0; align-items: flex-end;">
         <div class="ca-abo-contenedorBackRight" style="border-bottom: none; width: 100%;">
-          <div class="ca-abo-equipoRight ca-abo-equipoA" style="right: initial; left: -2px;">Ganador Lado Derecho</div>
+          <div class="ca-abo-equipoRight ca-abo-equipoA" style="right: initial; left: -2px;"><?php echo $ganadorLadoB ?></div>
         </div>
       </div>
       <div class="ca-abo-round ca-abo-round2" style="padding: 20px 0;">
         <div class="ca-abo-contenedorBackRight ca-abo-contenedorBack2">
-          <div class="ca-abo-equipoRight ca-abo-equipoA" style="right: 10px;">Ganador Ronda 1</div>
-          <div class="ca-abo-equipoRight ca-abo-equipoB" style="right: 10px;">Ganador Ronda 2</div>
+          <div class="ca-abo-equipoRight ca-abo-equipoA" style="right: 10px;"><?php echo $ganadorPartido3 ?></div>
+          <div class="ca-abo-equipoRight ca-abo-equipoB" style="right: 10px;"><?php echo $ganadorPartido4 ?></div>
         </div>
       </div>
       <div class="ca-abo-round ca-abo-round1" style="padding: 20px 0;">
         <div class="ca-abo-contenedorBackRight ca-abo-contenedorBack1">
-          <div class="ca-abo-equipoRight ca-abo-equipoA" style="right: -1px;">Quinto Equipo</div>
-          <div class="ca-abo-equipoRight ca-abo-equipoB" style="right: -1px;">Sexto Equipo</div>
+          <div class="ca-abo-equipoRight ca-abo-equipoA" style="right: -1px;"><?php echo $equipo5 ?></div>
+          <div class="ca-abo-equipoRight ca-abo-equipoB" style="right: -1px;"><?php echo $equipo6 ?></div>
         </div>
         <div class="ca-abo-contenedorBackRight ca-abo-contenedorBack1">
-          <div class="ca-abo-equipoRight ca-abo-equipoA" style="right: -1px;">Septimo Equipo</div>
-          <div class="ca-abo-equipoRight ca-abo-equipoB" style="right: -1px;">Octavo Equipo</div>
+          <div class="ca-abo-equipoRight ca-abo-equipoA" style="right: -1px;"><?php echo $equipo7 ?></div>
+          <div class="ca-abo-equipoRight ca-abo-equipoB" style="right: -1px;"><?php echo $equipo8 ?></div>
         </div>
       </div>
     </div>
@@ -173,5 +194,11 @@
   }
 
   .ca-abo-center { grid-area: 3; }
+}
+
+@media (max-width: 439px) {
+  .ca-abo-web {
+    display: none;
+  }
 }
 </style>
